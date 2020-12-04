7d962e9b8342728a80cf23ed51be09dc3bf7c8f6
manbearpear <74177003+manbearpear@users.noreply.github.com>
2020-12-04 10:45:35 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
     
+    
+    
     // SCROLLED HEADER
 window.addEventListener('scroll', function () {
     document.body.classList[
@@ -10,7 +12,7 @@ window.addEventListener('scroll', function () {
 
     // MOBILNAVIGATION
     function myFunction() {
-        var x = document.getElementById("myTopnav");
+        var x = document.getElementById("header");
         if (x.className === "topnav") {
           x.className += " responsive";
         } else {
